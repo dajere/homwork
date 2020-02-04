d31a4ad9740a81f8c7e9d786cc61ddce091ffe95
dajere newby <d.newby@setonhill.edu>
2020-02-04 12:04:36 -0500

    Initial Commit

---
+++ homwork/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  homwork
+//
+//  Created by dajere kyree newby on 2/4/20.
+//  Copyright © 2020 dajere kyree newby. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
